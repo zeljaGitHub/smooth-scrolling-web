b20812c96951c0eb5a9a38d163b9271999d1e6f1
Zeljko <zel87king2@gmail.com>
2023-08-12 21:01:30 +0200

    video

--- src/components/component1.jsx
+++ src/components/component1.jsx
@@ -4,7 +4,7 @@ const Component1 = () => {
   return (
     <div className="full-screen c1">
       <video className="video" autoPlay loop muted>
-        <source src="/src/assets/sea.mp4" type="video/mp4" />
+        <source src="/sea.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
