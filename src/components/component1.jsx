c53d287cc172fb5428541b29309fe49fa8d27f41
Zeljko <zel87king2@gmail.com>
2023-08-15 22:48:59 +0200

    added lots of things

--- src/components/component1.jsx
+++ src/components/component1.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Component1 = () => {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    const videoElement = document.querySelector(".video");
+
+    const handleVideoLoad = () => {
+      setVideoLoaded(true);
+    };
+
+    videoElement.addEventListener("loadeddata", handleVideoLoad);
+
+    return () => {
+      videoElement.removeEventListener("loadeddata", handleVideoLoad);
+    };
+  }, []);
+
   return (
     <div className="full-screen c1">
+      {!videoLoaded && <div className="loading">Loading Video...</div>}
       <video className="video" autoPlay loop muted>
         <source src="/sea.mp4" type="video/mp4" />
         Your browser does not support the video tag.
--- src/components/component2.jsx
+++ src/components/component2.jsx
@@ -1,7 +1,165 @@
 import React from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 
 const Component2 = () => {
-  return <div className="full-screen c2">Component 2</div>;
+  async function loadParticles(main) {
+    await loadSlim(main);
+  }
+
+  return (
+    <>
+      <div className="full-screen">
+        <Particles
+          className="full-screen-p"
+          id="tsparticles"
+          init={loadParticles}
+          options={{
+            autoPlay: true,
+            background: {
+              color: {
+                value: "#000",
+              },
+              image: "",
+              position: "",
+              repeat: "",
+              size: "",
+              opacity: 1,
+            },
+
+            fullScreen: {
+              enable: false,
+              zIndex: -1,
+            },
+
+            fpsLimit: 120,
+
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: "#111",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "111",
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: "111",
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+              },
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "grab",
+                },
+                onclick: {
+                  enable: false,
+                  mode: "push",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 215.77042027282963,
+                  line_linked: {
+                    opacity: 0.501821125050587,
+                  },
+                },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+            background: {
+              color: "74A89B",
+            },
+
+            pauseOnBlur: true,
+            pauseOnOutsideViewport: true,
+            responsive: [],
+            smooth: false,
+            style: {},
+            themes: [],
+            zLayers: 100,
+            emitters: [],
+          }}
+        />
+        <p className="c2">Component 2</p>
+      </div>
+    </>
+  );
 };
 
 export default Component2;
